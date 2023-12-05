c71dacf3d2ce00a737b04fdb83cfa122ed2c4f66
jexiaong <jexiaong@umich.edu>
2023-12-05 12:52:54 -0500

    code

--- foreca.py
+++ foreca.py
@@ -3,7 +3,8 @@ import requests
 # Replace 'YourAPIKey' and 'YourLocationKey' with your actual AccuWeather API key and location key
 api_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTcwMTgzMjY1NCwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE3MDE4MzI2NTQsImp0aSI6ImM4NTAxYmUwNjQyNjg5NTEiLCJzdWIiOiJqZXhpYW9uZyIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.MNoeWRihos1dmkFQN93nWb4QfG5xrvz3LCSkUX-qgpc'
 api_key2 = 'aUE9PSJ9.MNoeWRihos1dmkFQN93nWb4QfG5xrvz3LCSkUX-qgpc'
-location = '42.2808, 83.7430'
+location = 102643743 #'42.2808, 83.7430'
+
 
 # URL for the hourly forecast API
 url = f"https://api.foreca.com/api/v1/forecast/hourly/:{location}?apikey={api_key}"
--- visualcrossing.py
+++ visualcrossing.py
@@ -1,35 +1,30 @@
 import requests
 import sqlite3
 import sys
+import json
 
-def create_table():
-    connection = sqlite3.connect('weather_data.db')
-    cursor = connection.cursor()
-
-    cursor.execute('''
+def create_table(conn, cur):
+    cur.execute('''
         CREATE TABLE IF NOT EXISTS visualcrossing (
-            datetime INTEGER PRIMARY KEY,
+            datetimeEpoch INT PRIMARY KEY,
             temp REAL,
             feelslike REAL,
             humidity REAL,
-            precip TEXT,
+            precip REAL,
             precipprob REAL,
-            snow TEXT,
+            snow REAL,
             snowdepth REAL,
-            preciptype TEXT,
             windgust REAL,
             windspeed REAL,
             winddir REAL,
             pressure REAL,
             visibility REAL,
             cloudcover REAL,
-            conditions TEXT,
-            UNIQUE(datetime,)
+            conditions TEXT
         )
     ''')
+    conn.commit()
 
-    connection.commit()
-    connection.close()
 
 response = requests.request("GET", "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Ann%20Arbor%2C%20Michigan?unitGroup=metric&include=hours&key=DXM3T4BUXTSL3WQ47M2PKACHD&contentType=json")
 
@@ -38,46 +33,57 @@ if response.status_code != 200:
     sys.exit()
 
 jsonData = response.json()
+# print(jsonData)
+# print(json.dumps(jsonData, indent=2)[:1000*2])
+
+connection = sqlite3.connect('weather_data.db')
+cursor = connection.cursor()
 
 #Create table if it doesn't already exist
-create_table()
+create_table(connection, cursor)
 
-for hourly_data in jsonData['hours']:
-    datetime = hourly_data['datetime']
-    temp = hourly_data['temp']
-    feelslike = hourly_data['feelslike']
-    humidity = hourly_data['humidity']
-    precip = hourly_data['precip']
-    precipprob = hourly_data['precipprob']
-    snow = hourly_data['snow']
-    snowdepth = hourly_data['snowdepth']
-    preciptype = hourly_data['preciptype']
-    windgust = hourly_data['windgust']
-    windspeed = hourly_data['windspeed']
-    winddir = hourly_data['datetwinddirime']
-    pressure = hourly_data['pressure']
-    visibility = hourly_data['visibility']
-    cloudcover = hourly_data['cloudcover']
-    conditions = hourly_data['conditions']
+count = 0
+for day in jsonData['days']:
+    for hourly_data in day['hours']:
+        if count < 25:
+            count += 1
+            datetimeEpoch = hourly_data['datetimeEpoch']
+            # print(datetimeEpoch)
+            temp = hourly_data['temp']
+            feelslike = hourly_data['feelslike']
+            humidity = hourly_data['humidity']
+            precip = hourly_data['precip']
+            precipprob = hourly_data['precipprob']
+            snow = hourly_data['snow']
+            snowdepth = hourly_data['snowdepth']
+            windgust = hourly_data['windgust']
+            windspeed = hourly_data['windspeed']
+            winddir = hourly_data['winddir']
+            pressure = hourly_data['pressure']
+            visibility = hourly_data['visibility']
+            cloudcover = hourly_data['cloudcover']
+            conditions = hourly_data['conditions']
 
-    connection = sqlite3.connect('weather_data.db')
-    cursor = connection.cursor()
+            # Check if the data already exists in the table
+            cursor.execute('SELECT datetimeEpoch FROM visualcrossing WHERE datetimeEpoch = ?', (datetimeEpoch,))
+            existing_data = cursor.fetchone()
 
-    # Check if the data already exists in the table
-    cursor.execute('SELECT datetime FROM weather WHERE datetime = ?', (datetime,))
-    existing_data = cursor.fetchone()
+            if not existing_data:
+                cursor.execute('''
+                    INSERT INTO visualcrossing 
+                    (datetimeEpoch, temp, feelslike, humidity, precip, precipprob, snow, snowdepth, windgust,
+                    windspeed, winddir, pressure, visibility, cloudcover, conditions) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''',
+                    (datetimeEpoch, temp, feelslike, humidity, precip, precipprob, 
+                    snow, snowdepth, windgust, windspeed, winddir, 
+                    pressure, visibility, cloudcover, conditions))
+                connection.commit()
 
-    if not existing_data:
-        cursor.execute('''
-                       INSERT INTO weather (datetime, temp, feelslike, humidity, 
-                       precip, precipprob, snow, snowdepth, preciptype, windgust,
-                       windspeed, winddir, pressure, visibility, cloudcover, conditions) 
-                       VALUES (?, ?, ?, ?)
-                       ''',
-                       (location, timestamp, temperature, precipitation))
-        connection.commit()
-        print(f'Data for {location} at {timestamp} inserted successfully.')
-    else:
-        print(f'Data for {location} at {timestamp} already exists in the database.')
+cursor.execute('''
+    SELECT COUNT(*) FROM visualcrossing
+''')
+rows = cursor.fetchone()[0]
+print(rows)
 
-    connection.close()
+connection.close()
